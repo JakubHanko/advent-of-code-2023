1d0aa4bcc9d34d02bdb97b5f49e6495b771f3539
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2023-12-09 14:51:57 +0100

    Add template file.

---
+++ src/main/scala/dayXX.scala
@@ -0,0 +1,11 @@
+package aoc.dayXX
+
+def solve1(input: Iterator[String]): Int =
+  ???
+
+def solve2(input: Iterator[String]): Int =
+  ???
+
+@main def main: Unit =
+  println(solve1(aoc.getInput(???)))
+  println(solve2(aoc.getInput(???)))

329d440cd41c558681b67161f4b1aca5f72c62be
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2023-12-17 13:05:25 +0100

    Add day 5, part 2

--- src/main/scala/day05.scala
+++ src/main/scala/day05.scala
@@ -11,10 +11,32 @@ def parseSeeds(line: String): Array[Seed] =
   }
 
 case class Mapping(range: SeedRange, to: Seed) {
+  def intersect(r: SeedRange): Boolean =
+    range.contains(r.start) || range.contains(r.end)
+
+  def getIntersect(r: SeedRange): Option[SeedRange] =
+    val intersectStart = math.max(range.start, r.start)
+    val intersectEnd = math.min(range.end, r.end)
+    if (intersectStart < intersectEnd)
+      Some(
+        intersectStart until intersectEnd
+      )
+    else None
+
   def apply(n: Seed): Seed = to + n - range.start
-  // TODO: split ranges if needed
-  // TODO2: consider part one with ranges of size 1?
-  def apply(r: SeedRange): List[SeedRange] = ???
+  def apply(r: SeedRange): (Option[SeedRange], List[SeedRange]) =
+    getIntersect(r) match
+      case None => (None, List(r))
+      case Some(intersection) =>
+        val toRange =
+          (to + intersection.head - range.start) until (to + intersection.last - range.start + 1)
+        var res: List[SeedRange] = List()
+        if (r.start < intersection.head)
+          res = (r.start until intersection.head) :: res
+        if (r.end > intersection.last + 1)
+          res = (intersection.last + 1 until r.end) :: res
+
+        (Some(toRange), res)
 }
 
 def createMapping(values: Array[Seed]): Mapping =
@@ -37,7 +59,8 @@ def parseMaps(lines: Iterator[String]): List[List[Mapping]] =
       .takeWhile(!_.isBlank)
       .map(_.split("\\s+").map(_.trim.toLong))
       .map(createMapping)
-      .toList :: parseMaps(lines)
+      .toList
+      .sortBy(_.range.start) :: parseMaps(lines)
 
 def parse(input: Iterator[String]): (Array[Seed], List[List[Mapping]]) =
   (parseSeeds(input.take(1).toList.head), parseMaps(input.drop(1)))
@@ -60,19 +83,29 @@ def createSeedRanges(seeds: Array[Seed]): List[SeedRange] =
     }
     .toList
 
+def splitRanges(
+    ranges: (List[SeedRange], List[SeedRange]),
+    map: Mapping
+): (List[SeedRange], List[SeedRange]) =
+  val (mapped, unmapped) = ranges
+  val (newMapped, newUnmapped) =
+    unmapped.map(map.apply(_)).unzip
+
+  (mapped ++ newMapped.flatten, newUnmapped.flatten)
+
 // TODO: refactor to use better abstractions when the AoC is over
 def solve2(input: Iterator[String]): Seed =
   val (seeds, mappings) = parse(input)
 
   val seedRanges = createSeedRanges(seeds)
   val locations = mappings.foldLeft(seedRanges): (seedRanges, maps) =>
-    maps.foldLeft(seedRanges): (seedRanges, map) =>
-      seedRanges.map(range => map.apply(range)).flatten
-      ???
-      ???
+    maps
+      .foldLeft((List(): List[SeedRange], seedRanges))(splitRanges)
+      .toList
+      .flatten
 
-  ???
+  locations.map(_.start).min
 
 @main def main: Unit =
   println(solve1(aoc.getInput(5)))
-  println(solve2(aoc.getInput(5, true)))
+  println(solve2(aoc.getInput(5)))
